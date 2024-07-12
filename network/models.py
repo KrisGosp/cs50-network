d385a2d64556ac9bc7170d6385180ef252f82ef2
Kris Gospodinov <kris060601@gmail.com>
2024-07-12 18:41:55 +0300

    Starter Post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.TextField(max_length=128)
+    body = models.TextField(max_length=1024)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0)
