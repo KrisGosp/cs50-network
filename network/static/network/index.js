3ada13fe0f874fe4a29af4037facbb6bb42b6da0
Kris Gospodinov <kris060601@gmail.com>
2024-07-12 22:18:03 +0300

    Implemented Alert Message

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector("#open-create-form").onclick = open_create_form;
+  document.querySelector("#open-create-form").onclick = toggle_create_form;
   document.querySelector("#create-form").onsubmit = create_post;
 });
 
-function open_create_form() {
-  document.querySelector("#create-post-form").className = "d-block";
-  document.querySelector("#open-create-form").className = "d-none";
+function toggle_create_form() {
+  document.querySelector("#create-post-form").classList.toggle("d-block");
+  document.querySelector("#open-create-form").classList.toggle("d-none");
+  document.querySelector("#title").value = "";
+  document.querySelector("#body").value = "";
+}
+
+function alert(message, type = "primary") {
+  alert = document.querySelector(".alert");
+  alert.innerHTML = message;
+  alert.classList.remove("d-none");
+  alert.classList.add(`alert-${type}`);
+  setTimeout(function () {
+    alert.classList.add("d-none");
+  }, 3000);
 }
 
 function create_post() {
@@ -21,11 +33,8 @@ function create_post() {
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.message);
-      title = "";
-      body = "";
-      document.querySelector("#open-create-form").className = "d-block";
-      document.querySelector("#create-post-form").className = "d-none";
+      alert(data.message, "success");
+      toggle_create_form();
     });
 
   return false;
